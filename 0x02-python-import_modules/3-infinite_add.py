7c831f67e18645ff334bff98259a4cb101bd79cd
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2022-12-07 13:34:45 -0800

    Write a program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        my_list.reverse()
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+if __name__ == "__main__":
+    from sys import argv
+
+    sum = 0
+    for arg in argv[1:]:
+        sum += int(arg)
+
+    print(sum)
