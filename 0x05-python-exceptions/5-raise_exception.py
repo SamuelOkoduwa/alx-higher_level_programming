e2ffb82e8276cea144e8dd967414d3a385185476
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-04-03 22:38:16 +0100

    Write a function that raises a type exception.

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    # Raise a type exception
+    raise TypeError("Type exception raised")
+
+'''# Sample test as it is in the example in the intranet
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
+'''
