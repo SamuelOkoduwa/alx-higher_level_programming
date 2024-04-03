9d794df924dc765645cd42046b2360a6d7031fac
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-04-03 22:41:21 +0100

    A function that raises a name exception with a message.

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+def raise_exception_msg(message=""):
+    # Raise a name exception with the provided message
+    raise NameError(message)
+
+'''# Sample usage from the intranent
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)'''
