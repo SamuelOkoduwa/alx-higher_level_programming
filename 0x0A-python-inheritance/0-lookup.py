22f5b134357f4a4a1cd2f14fa5b6306e45239514
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-04-18 16:19:46 +0100

    modified description

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,9 +2,9 @@
 
 def lookup(obj):
     attributes_and_methods = []
-    # Get the attributes and methods of the object
+    """ Get the attributes and methods of the object """
     for attribute in dir(obj):
-        # Exclude built-in attributes and methods starting with '__'
+        """Exclude built-in attributes and methods starting with '__'"""
         if not attribute.startswith('__'):
             attributes_and_methods.append(attribute)
     return attributes_and_methods
