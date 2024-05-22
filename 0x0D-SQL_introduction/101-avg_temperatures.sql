e781c0502bddb31e5a7a300089543caf75a7e50a
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-05-23 00:01:07 +0100

    a script that displays the average temperature

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- importing data from the file 'temperatures.csv' and writing a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+
+SELECT city, AVG(temperature) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
