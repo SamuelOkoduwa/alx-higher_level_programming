f8abd58a6308e60ac954688c8f1ec3338ede96a7
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-05-23 00:33:41 +0100

    a script that lists priviledges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- A script that lists all the privileges of the MYSQL user use_0d_1 and the privileges of the database use_0d_1
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
