865fe73c40db41226f8e537b2c95426ff721c46e
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-05-22 22:53:03 +0100

    modify the query from full tables to create tables

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- A script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
-SHOW FULL COLUMNS FROM hbtn_0c_0.first_table;
+SHOW CREATE TABLE FROM hbtn_0c_0.first_table;
