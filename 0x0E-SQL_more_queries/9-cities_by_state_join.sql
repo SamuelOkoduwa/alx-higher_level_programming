ed12af0105028f524171edc114246e379b25abb1
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-05-23 01:07:06 +0100

    a script that lists all cities contained in the database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,12 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa
 
-SELECT cities.name, states.name
-  FROM cities
-  JOIN states ON cities.state_id = states.id
- ORDER BY cities.id;
+-- SELECT cities.name, states.name
+--   FROM cities
+--   JOIN states ON cities.state_id = states.id
+--  ORDER BY cities.id;
+
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
