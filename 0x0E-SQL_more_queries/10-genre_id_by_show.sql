f1d030173d892511d2a39633af3ab3e885ca0734
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-05-23 01:12:22 +0100

    importing from a database and a script that lists all shows contained in a named database

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,11 +1,17 @@
 -- Import from a database and write a script that shows the genre_id by shows contained in the database hbtn_0d_tvshows that can be found in the database hbtn_0d_tvshows
 
-CREATE DATABASE
-	IF NOT EXISTS `hbtn_0d_tvshows`;
-USE `hbtn_0d_tvshows`;
-SELECT
-	`shows`.`title`,
-	`genres`.`name`
-  FROM `shows`
-  JOIN `genres` ON `shows`.`genre_id` = `genres`.`id`	
-ORDER BY `shows`.`id`;
+-- CREATE DATABASE
+-- 	IF NOT EXISTS `hbtn_0d_tvshows`;
+-- USE `hbtn_0d_tvshows`;
+-- SELECT
+-- 	`shows`.`title`,
+-- 	`genres`.`name`
+--   FROM `shows`
+--   JOIN `genres` ON `shows`.`genre_id` = `genres`.`id`	
+-- ORDER BY `shows`.`id`;
+
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
