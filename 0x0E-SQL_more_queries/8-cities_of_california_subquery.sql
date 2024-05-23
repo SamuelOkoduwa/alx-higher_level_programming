8cfb9d96ef6c3d8d8433afe2ccbe1226484e283b
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-05-23 01:02:56 +0100

    a script that lists all the cities in Califonia

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- a script that lists all the cities in California that can be found in the database hbtn_0d_usa
+
+SELECT cities.name	FROM cities
+JOIN states ON cities.state_id = states.id
+WHERE states.name = 'California';
