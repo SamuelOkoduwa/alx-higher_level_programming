a14ee82259e4ccb2add2aa231f3a69691daee195
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-05-23 15:09:05 +0100

    a script that uses a database and list all records

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- Importing from a database and writing a script that uses the hbtn_0d_tvshows database to list all genres of the show Dexter
+
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
