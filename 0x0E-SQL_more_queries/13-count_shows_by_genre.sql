bf3b5079510b7a378b400676bfe7be1bc8b58a72
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-05-23 01:27:04 +0100

    a script that lists from a database

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,16 @@
 -- Import from a database and write a script that lists all genres from hbtn_0d_tvshows and displays
 
-SELECT g.`id`, g.`name`, COUNT(s.`id`) AS `number_of_shows`
-  FROM `genres` AS g
-	   LEFT JOIN `shows` AS s
-	   ON g.`id` = s.`genre_id`
- GROUP BY g.`id`, g.`name`
- ORDER BY g.`id`;
+-- SELECT g.`id`, g.`name`, COUNT(s.`id`) AS `number_of_shows`
+--   FROM `genres` AS g
+-- 	   LEFT JOIN `shows` AS s
+-- 	   ON g.`id` = s.`genre_id`
+--  GROUP BY g.`id`, g.`name`
+--  ORDER BY g.`id`;
+
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
