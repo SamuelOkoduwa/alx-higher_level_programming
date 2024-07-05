0453602891db19974284fc0afc86b244343839fd
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-07-05 13:01:35 +0100

    modified

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response only if the status code is 200
+response=$(curl -s -w "%{http_code}" "$1")
+body=${response::-3}
+status_code=${response: -3}
+
+if [ "$status_code" -eq 200 ]; then
+  echo "$body"
+fi
