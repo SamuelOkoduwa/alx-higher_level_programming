bb7c48b36778ed9da166944f2a5f48e5997e7de0
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-07-05 13:18:57 +0100

    modifying lines

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a POST request to the URL, and displays the body of the response
-# A variable email must be sent with the value test@gmail.com
-# A variable subject must be sent with the value I will always be here for PLD
+# A variable email must be sent with the value test@gmail.com and A variable subject must be sent with the value I will always be here for PLD
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
