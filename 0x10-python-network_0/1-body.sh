be7117d84b666c2d49ff76dc703f9a97d7711d9e
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-07-07 13:02:29 -0700

    The modified lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL, and displays the body of the response only if the status code is 200
-response=$(curl -s -w "%{http_code}" "$1")
-body=${response::-3}
-status_code=${response: -3}
-
-if [ "$status_code" -eq 200 ]; then
-  echo "$body"
-fi
-
+# This script sends a GET request to the URL and displays the body of the response only if the status code is 200
+curl -sL -X GET "$1"
